168d8115fa0080f7de39ec5dc6b844262c263514
Andre Carlos <contato@andrecarlos.com.br>
2019-12-12 19:39:36 -0200

    Reviews creation

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -10,6 +10,7 @@ import { Restaurant } from './restaurant/restaurant.model';
 
 import { MEAT_API } from '../app.api';
 import { ErrorHandler } from '../app.error-handler';
+import { MenuItem } from 'app/restaurant-detail/menu-item/menu-item.model';
 
 
 @Injectable()
@@ -26,6 +27,20 @@ export class RestaurantsService {
 
   RestaurantById(id: string): Observable<Restaurant> {
     return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+
+  reviewsOfRestaurant(id: string): Observable<any> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
+  }
+
+
+  menuOfRestaurant(id: string): Observable<MenuItem[]> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}/menu`)
       .map(response => response.json())
       .catch(ErrorHandler.handleError);
   }
