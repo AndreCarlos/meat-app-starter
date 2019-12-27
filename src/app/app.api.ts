14e2f5c80426387bc95c197b8596c14710a4daca
Andre Carlos <contato@andrecarlos.com.br>
2019-12-26 22:01:42 -0200

    Página not-found e ng build

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,2 +1,6 @@
- // tslint:disable-next-line:eofline
- export const MEAT_API = 'http://localhost:3000';
+import { environment } from '../environments/environment.prod';
+
+
+// tslint:disable-next-line:eofline
+//  export const MEAT_API = 'http://localhost:3000';
+export const MEAT_API = environment.api;
